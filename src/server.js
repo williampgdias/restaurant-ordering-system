7d9311355cf24d4b2c2fa959fcd9df5c62936abe
William Dias <smdwilliam89@gmail.com>
2025-06-18 16:35:20 -0300

    feat: Installing Mongoose.

--- src/server.js
+++ src/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const mongoose = require('mongoose');
 
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+// Connect to MongoDB
+mongoose
+    .connect(DATABASE_URL)
+    .then(() => {
+        console.log('Connected to MongoDB successfully!');
+    })
+    .catch((err) => {
+        console.error('Failed to connect to MongoDB:', err.message);
+        process.exit(1);
+    });
 
 app.get('/', (req, res) => {
     res.send('Welcome to the Restaurant Ordering System API!');
